21981b8d789ec577f9cc3814e2c7aae905fc4135
Felipe Weber <felipe_weber@terceiros.sicredi.com.br>
2021-04-22 11:25:30 -0300

    first commit

---
+++ Project1/Project1.h
@@ -0,0 +1,18 @@
+//
+//  Project1.h
+//  Project1
+//
+//  Created by Felipe Bayer Weber on 22/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Project1.
+FOUNDATION_EXPORT double Project1VersionNumber;
+
+//! Project version string for Project1.
+FOUNDATION_EXPORT const unsigned char Project1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Project1/PublicHeader.h>
+
+
